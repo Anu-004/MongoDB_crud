cc9c9f13edb1ca13b62c95e92d9c36c4a5d6603b
Anu <138310483+Anu-004@users.noreply.github.com>
2024-11-06 11:55:45 +0530

    Update cors @server.js

--- server.js
+++ server.js
@@ -8,9 +8,16 @@ dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 app.use(express.json());
-app.use(cors()); // Enable CORS for all routes
+// app.use(cors()); 
+// // Enable CORS for all routes
 
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'vercellink', // Replace with her actual frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// app.use(express.urlencoded({ extended: true }));
 
 // MongoDB connection
 const uri = process.env.MONGODB_URI;
